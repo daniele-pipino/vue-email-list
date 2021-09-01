a897c00e5cb346e4d0c6c78aa735f9832d9a75c2
daniele-pipino <Danielemacpipino@icloud.com>
2021-09-01 17:50:42 +0200

    printed email

--- js/main.js
+++ js/main.js
@@ -1,5 +1,23 @@
+// vue config tools
+
+Vue.config.devtools = true;
+
+// inizializazzione vue js
 const app = new Vue({
     el: '#app',
-    data: {},
+    data: {
+        email: [],
+    },
     methods: {},
-})
+    created() {
+        for (let i = 0; i < 10; i++) {
+            axios
+                .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((res) => {
+                    const result = res.data.response;
+                    this.email.push(result);
+                    console.log(this.email);
+                });
+        }
+    }
+});
